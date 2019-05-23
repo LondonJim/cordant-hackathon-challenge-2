e1899fbeac10fbf8b7ab6dbd44e356eed37efaa0
James Sutherland <jrsutherland78@googlemail.com>
2019-05-23 13:21:09 +0100

    pass client to .ManagerFinder, access requester api - James

--- app/app.js
+++ app/app.js
@@ -1,23 +1,17 @@
-function runPasswordFinder(requester, cl) {
-  managerFinder = new ManagerFinder
-  managerFinder.execute(requester, cl)
+
+function runPasswordFinder(client) {
+  managerFinder = new ManagerFinder(client)
+  managerFinder.execute()
 }
 
 $(document).ready( function() {
     app.initialized()
         .then(function(_client) {
-          var client = _client;
+          let client = _client;
           client.events.on('app.activated',
             function() {
-              let cl = client
-                client.data.get('requester')
-                    .then(function(data) {
-                        $('#apptext').text("Ticket created by " + data.requester.name);
-                        runPasswordFinder(data.requester.id, cl)
-                    })
-                    .catch(function(e) {
-                        console.log('Exception - ', e);
-                    });
+              runPasswordFinder(client)
+            
         });
     });
 });
--- app/src/managerFinder.js
+++ app/src/managerFinder.js
@@ -1,13 +1,21 @@
 class ManagerFinder {
 
-  constructor(requesterId) {
-    this.requester = requester
+  constructor(client) {
+    this.client = client
     this.url = 'https://cordantgroup-helpdesk.freshservice.com/api/v2/requesters'
-    this.token = "<%= data.token %>"
+    this.headers = { "headers": { "x-auth-token": "<%= data.token %>"}}
+    this.requester
   }
 
   execute() {
-    request.post(this.url, this.token)
-    console.log(this.token)
+    let that = this
+    this.client.data.get('requester')
+        .then(function(data) {
+            $('#apptext').text("Ticket created by " + data.requester.name);
+            let outcome = that.client.request.get(that.url, that.headers)
+        })
+        .catch(function(e) {
+            console.log('Exception - ', e);
+        });
   }
 }
