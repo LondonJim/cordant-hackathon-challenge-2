6e6b3a3d482d70ae615d5a00e10f16b31b58b3ce
James Sutherland <jrsutherland78@googlemail.com>
2019-05-23 14:35:08 +0100

    Compared client id and API id - Daniel

--- app/src/managerFinder.js
+++ app/src/managerFinder.js
@@ -3,19 +3,31 @@ class ManagerFinder {
   constructor(client) {
     this.client = client
     this.url = 'https://cordantgroup-helpdesk.freshservice.com/api/v2/requesters'
-    this.headers = { headers: { "Authorization": "Basic <%= encode(iparam.data.api_key) %>"}}
-    this.requester
+    this.headers = { headers: { "Authorization": "Basic <%= encode(iparam.data.api_key) %>"}};
+    this.requesterId
+    this.results
+  }
+
+  findRequester() {
+    let that = this;
+    this.results.requesters.forEach(function(requester) {
+      if (requester.id === that.requesterId) {
+        console.log(requester);
+      }
+    })
   }
 
   execute() {
     let that = this
     this.client.data.get('requester')
         .then(function(data) {
-            $('#apptext').text("Ticket created by " + data.requester.name);
+            // $('#apptext').text("Ticket created by " + data.requester.name);
+            that.requesterId = data.requester.id;
             that.client.request.get(that.url, that.headers)
-              .then(function(result) {
-                console.log(result)
-              })
+              .then(function(results) {
+                that.results = JSON.parse(results.response)
+                that.findRequester()
+              });
         })
         .catch(function(e) {
             console.log('Exception - ', e);
